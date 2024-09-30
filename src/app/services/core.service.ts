94aae82e2e89a73682a3544ea7c73f4d2961b44c
mendexco <vitormendesco.js@gmail.com>
2024-09-30 20:49:02 -0300

    feat(app): adding services folder and core service

---
+++ src/app/services/core.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+export type SelectionType = 'X' | 'O' | null;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoreService {
+  constructor() {}
+}
