b805a8727c45fbb5699f4c783ef6a4f8e15bc4d4
mendexco <vitormendesco.js@gmail.com>
2024-09-30 20:49:57 -0300

    refactor(app): cleaning ocmponent and using app-panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelComponent } from '@components/panel/panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PanelComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tic-tac-toe-angular';
