270014bac159efaa25a5e47137d4d2c1e4aa2406
mendexco <vitormendesco.js@gmail.com>
2024-10-01 23:27:15 -0300

    feat(components): adding match-manager component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header/header.component';
+import { MatchManagerComponent } from '@components/match-manager/match-manager.component';
 import { PanelComponent } from '@components/panel/panel.component';
-import { MatchManagerComponent } from './components/match-manager/match-manager.component';
 
 @Component({
   selector: 'app-root',
--- src/app/components/match-manager/match-manager.component.ts
+++ src/app/components/match-manager/match-manager.component.ts
@@ -3,14 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-match-manager',
   standalone: true,
-  imports: [],
   template: `
     <nav>
-      match-manager works!
+      <div>
+        <p>
+          Player1 <span>{{ player1Wins }}</span> x
+          <span>{{ player2Wins }}</span> Player2
+        </p>
+      </div>
+      <button type="button">{{ isRunning ? 'RESTART' : 'START' }}</button>
     </nav>
   `,
-  styleUrl: './match-manager.component.scss'
+  styleUrl: './match-manager.component.scss',
 })
 export class MatchManagerComponent {
-
+  isRunning = false;
+  player1Wins = 0;
+  player2Wins = 0;
 }
